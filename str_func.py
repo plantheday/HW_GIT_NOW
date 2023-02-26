5b895defa31e259f795444c2f6474ce924281807
plantheday <125719231+plantheday@users.noreply.github.com>
2023-02-26 20:17:27 +0300

    Update str_func.py
    add docstring

--- str_func.py
+++ str_func.py
@@ -1,4 +1,7 @@
 def up_the_letters():
+    """
+    Принимает на вход строку и возвращает ее со всеми заглавными буквами
+    """
     user_input = input("Вам нужно тут что-то написать")
     up_words = user_input.upper()
     print(up_words)
